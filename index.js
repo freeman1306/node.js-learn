0cfdea89f3b0ea1469db7489394e63026dd39085
freeman1306 <zavalkin06@gmail.com>
2020-05-20 00:38:47 +0300

    comit

--- index.js
+++ index.js
@@ -5,31 +5,59 @@ const path = require('path')
 const server = http.createServer((req, res) => {
    
 
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-            if (err) {
-               throw err
-            }
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+    //         if (err) {
+    //            throw err
+    //         }
 
-            res.writeHead(200, {
-                'content-type': 'text/html'
-            })
-            res.end(data)
-       })
-    }
+    //         res.writeHead(200, {
+    //             'content-type': 'text/html'
+    //         })
+    //         res.end(data)
+    //    })
+    // }
     
-    if (req.url === '/contact') {
-        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-            if (err) {
-               throw err
-            }
+    // if (req.url === '/contact') {
+    //     fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+    //         if (err) {
+    //            throw err
+    //         }
 
+    //         res.writeHead(200, {
+    //             'content-type': 'text/html'
+    //         })
+    //         res.end(data)
+    //    })
+    // }
+
+    let filepath = path.join(__dirname, 'public', (req.url === '/' ? 'index.html' : req.url))
+
+    console.log(filepath);
+
+    fs.readFile(filepath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) { 
+                    res.writeHead(500).end("Error")
+                } else {
+                    res.writeHead(200, {
+                        'content-type': 'text/html'
+                    })
+                    res.end(data)
+                 }
+                
+            })
+        } else {
             res.writeHead(200, {
                 'content-type': 'text/html'
             })
-            res.end(data)
-       })
-    }
+            res.end(content)
+        }
+        
+    })
+
+   
 })
 
 
